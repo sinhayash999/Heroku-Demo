8ce30ff3d5d1f6587ae7b8195870f90bf75bdfa7
Yash Sinha <50780121+sinhayash999@users.noreply.github.com>
2021-04-17 19:50:49 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+# Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -13,14 +14,19 @@ X = dataset.iloc[:, :3]
 
 y = dataset.iloc[:, -1]
 
+#Splitting Training and Test Set
+#Since we have a very small dataset, we will train our model with all availabe data.
+
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 
+#Fitting model with trainig data
 regressor.fit(X, y)
 
+# Saving model to disk
 pickle.dump(regressor, open('model.pkl','wb'))
 
+# Loading model to compare the results
 model = pickle.load(open('model.pkl','rb'))
-print(model.predict([[8, 9, 7]]))
-
+print(model.predict([[2, 9, 6]]))
 
