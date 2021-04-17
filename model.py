8f01ee3d6804ac4d74f8978c9ccf0e5d88547a00
Yash Sinha <50780121+sinhayash999@users.noreply.github.com>
2021-04-17 13:54:37 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import pickle
+
+dataset = pd.read_csv('hiring.csv')
+
+dataset['experience'].fillna(0, inplace=True)
+
+dataset['test_score'].fillna(dataset['test_score'].mean(), inplace=True)
+
+X = dataset.iloc[:, :3]
+
+y = dataset.iloc[:, -1]
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+
+regressor.fit(X, y)
+
+pickle.dump(regressor, open('model.pkl','wb'))
+
+model = pickle.load(open('model.pkl','rb'))
+print(model.predict([[8, 9, 7]]))
+
+
